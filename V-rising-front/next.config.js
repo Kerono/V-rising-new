04730179f8ad8123db88a1d5d2298f3f1369ac57
Evgeniy <shtk.pno@yandex.ru>
2026-08-02 14:51:48 +0300

    workflows-ci

--- V-rising-front/next.config.js
+++ V-rising-front/next.config.js
@@ -1,8 +1,15 @@
+//TODO improve
+const SERVER_BACKEND_URL =
+  process.env.NEXT_PUBLIC_SERVER_BACKEND_URL || "http://backend:3000/";
+
+const CLIENT_BACKEND_URL =
+  process.env.NEXT_PUBLIC_CLIENT_BACKEND_URL || "http://localhost:3000/**";
+
 const nextConfig = {
   images: {
     remotePatterns: [
-      new URL(`${process.env.NEXT_PUBLIC_SERVER_BACKEND_URL}**`),
-      new URL(`${process.env.NEXT_PUBLIC_CLIENT_BACKEND_URL}**`),
+      new URL(`${SERVER_BACKEND_URL}**`),
+      new URL(`${CLIENT_BACKEND_URL}**`),
     ],
   },
 };
